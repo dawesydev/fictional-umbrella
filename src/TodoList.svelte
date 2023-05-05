5a76fa2f0d490252146e7cc690a8764c5e4c2e1a
Dan <danieldawes@protonmail.com>
2023-05-05 10:48:01 +0100

    fix: dbclick to edit

--- src/TodoList.svelte
+++ src/TodoList.svelte
@@ -1,139 +1,140 @@
 <script>
-// @ts-nocheck
-
-    import { fade, fly } from 'svelte/transition'
-    import { createEventDispatcher } from 'svelte'
-
-    const dispatch = createEventDispatcher()
-
-    // define props using 'export'
-    export let filteredTodos
-    export let remainingTodos
-    export let currentFilter
-
+    import { fade, fly } from "svelte/transition";
+  
+    export let filteredTodos;
+    export let remainingTodos;
+    export let currentFilter;
+  
+    import { createEventDispatcher } from "svelte";
+  
+    const dispatch = createEventDispatcher();
+  
     function checkAllTodos() {
-        dispatch('checkAllTodosDispatched')
+      dispatch("checkAllTodosDispatched");
     }
+  
     function clearCompleted() {
-        dispatch('clearCompletedDispatched')
+      dispatch("clearCompletedDispatched");
     }
+  
     function deleteTodo(id) {
-        dispatch('deleteTodoDispatched', {
-            id,
-        })
+      dispatch("deleteTodoDispatched", {
+        id,
+      });
     }
+  
     function updateFilter(filter) {
-        dispatch('updateFilterDispatched', {
-            filter,
-        })
+      dispatch("updateFilterDispatched", {
+        filter,
+      });
     }
-
+  
     function editTodo(todo) {
-        dispatch('editTodo', {
-            todo,
-        })
+      dispatch("editTodoDispatched", {
+        todo,
+      });
     }
+  
     function doneEdit(todo) {
-        dispatch('doneEdit', {
-            todo,
-        })
+      dispatch("doneEditDispatched", {
+        todo,
+      });
     }
+  
     function doneEditKeydown(event, todo) {
-        dispatch('doneEditKeydown', {
-            key: event.key,
-            todo,
-        })
+      dispatch("doneEditKeydownDispatched", {
+        key: event.key,
+        todo,
+      });
     }
-</script>
-
-<div>
+  </script>
+  
+  <div>
     <ul class="todo-list">
-        {#each filteredTodos as todo (todo.id)}
-            <li
-                class="todo-item-container"
-                in:fly={{ x: 100, duration: 400 }}
-                out:fade={{ duration: 400 }}
+      {#each filteredTodos as todo (todo.id)}
+        <li
+          class="todo-item-container"
+          in:fly={{ x: 100, duration: 400 }}
+          out:fade={{ duration: 400 }}
+        >
+          <div class="todo-item">
+            <input type="checkbox" bind:checked={todo.isComplete} />
+            {#if !todo.isEditing}
+              <span
+                on:dblclick={editTodo(todo)}
+                class="todo-item-label"
+                class:line-through={todo.isComplete}>{todo.title}</span
+              >
+            {:else}
+              <input
+                type="text"
+                class="todo-item-input"
+                bind:value={todo.title}
+                on:blur={doneEdit(todo)}
+                on:keydown={(event) => doneEditKeydown(event, todo)}
+                autofocus
+              />
+            {/if}
+          </div>
+          <button class="x-button" on:click={deleteTodo(todo.id)}>
+            <svg
+              class="x-button-icon"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-                <div class="todo-item">
-                    <input type="checkbox" bind:checked={todo.isComplete} />
-                    {#if !todo.isEditing}
-                        <!-- content here -->
-                        <span
-                            on:dblclick={editTodo(todo)}
-                            class="todo-item-label"
-                            class:line-through={todo.isComplete}
-                            >{todo.title}</span
-                        >
-                    {:else}
-                        <!-- else content here -->
-
-                        <input
-                            type="text"
-                            class="todo-item-input"
-                            bind:value={todo.title}
-                            on:blur={doneEdit(todo)}
-                            autofocus
-                            on:keydown={(event) => doneEditKeydown(event, todo)}
-                        />
-                    {/if}
-                </div>
-                <button class="x-button" on:click={deleteTodo(todo.id)}>
-                    <svg
-                        class="x-button-icon"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M6 18L18 6M6 6l12 12"
-                        />
-                    </svg>
-                </button>
-            </li>
-        {/each}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </li>
+      {/each}
     </ul>
-
+  
     <div class="check-all-container">
-        <div>
-            <button on:click={checkAllTodos} class="button">Check All</button>
-        </div>
-        <div>
-            {#key remainingTodos}
-                <span style="display: inline-block" in:fly={{ y: -20 }}
-                    >{remainingTodos}</span
-                >
-            {/key}
-            <span>items remaining</span>
-        </div>
+      <div>
+        <button on:click={checkAllTodos} class="button">Check All</button>
+      </div>
+  
+      <!-- <span>{remainingTodos} items remaining</span> -->
+  
+      <div>
+        {#key remainingTodos}
+          <span style="display: inline-block" in:fly={{ y: -20 }}>
+            {remainingTodos}
+          </span>
+        {/key}
+        <span>items remaining</span>
+      </div>
     </div>
-
+  
     <div class="other-buttons-container">
-        <div>
-            <button
-                class="button filter-button"
-                class:filter-button-active={currentFilter === 'all'}
-                on:click={() => updateFilter('all')}>All</button
-            >
-            <button
-                class="button filter-button"
-                class:filter-button-active={currentFilter === 'all'}
-                on:click={() => updateFilter('active')}>Active</button
-            >
-            <button
-                class="button filter-button"
-                class:filter-button-active={currentFilter === 'all'}
-                on:click={() => updateFilter('completed')}>Completed</button
-            >
-        </div>
-        <div>
-            <button
-                on:click={clearCompleted}
-                class="button filter-button filter-button-active"
-                >Clear completed</button
-            >
-        </div>
+      <div>
+        <button
+          on:click={() => updateFilter("all")}
+          class="button filter-button"
+          class:filter-button-active={currentFilter === "all"}
+        >
+          All
+        </button>
+        <button
+          on:click={() => updateFilter("active")}
+          class="button filter-button"
+          class:filter-button-active={currentFilter === "active"}>Active</button
+        >
+        <button
+          on:click={() => updateFilter("completed")}
+          class="button filter-button"
+          class:filter-button-active={currentFilter === "completed"}
+          >Completed</button
+        >
+      </div>
+      <div>
+        <button class="button" on:click={clearCompleted}>Clear completed</button>
+      </div>
     </div>
-</div>
+  </div>
